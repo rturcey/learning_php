22a06be8fa3a252bd0cee7e640ccf00c3d6a5047
rturcey <richard.turcey@hotmail.fr>
2021-12-15 16:59:46 +0100

    d02ex02 ok

--- piscine_php/d02/ex02/magnifying_glass.php
+++ piscine_php/d02/ex02/magnifying_glass.php
@@ -1,4 +1,25 @@
 #!/usr/bin/php
 <?php
-    //this is shit, let's do better tomorrow
+    if (count($argv) < 2 || !$fp = fopen($argv[1], "r"))
+        return ;
+    while ($line = fgets($fp)) {
+        $line = preg_replace_callback(
+            '/(<a )(.*?)(>)(.*)(<\/a>)/',
+            function ($matches) {
+                $matches[0] = preg_replace_callback(
+                '/(>)(.*?)(<)/', function ($matches) {
+                    return $matches[1].strtoupper($matches[2]).$matches[3];
+                }, $matches[0]
+                );
+                $matches[0] = preg_replace_callback(
+                '/(<.*?title=[\"\'])(.*[\"\'])/', function ($matches) {
+                    return $matches[1].strtoupper($matches[2]);
+                }, $matches[0]
+                );
+                return $matches[0];
+            }, $line
+        );
+        echo $line;
+    }
+    fclose($fp);
 ?>
